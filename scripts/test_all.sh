98f3d2c7c6b2f56053ee8fde0443d7ce0012cdcf
Tiago Cardoso <tcardoso@gmail.com>
2017-09-28 22:03:52 +0800

    Small fix on test script

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -5,8 +5,7 @@ export MOCHA_BADGE_OK_COLOR=green
 export MOCHA_BADGE_KO_COLOR=orange
 export MOCHA_BADGE_STYLE=flat
 
-mocha 
 #Reporter
 mocha --reporter mocha-reporter-badge | sed -n -e '/<svg/,$p' > badge.svg
 
-
+mocha
